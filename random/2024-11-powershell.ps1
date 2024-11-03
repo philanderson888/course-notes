ef757da0d1ecddd1807b35eedef6d7e5de5ccf07
philanderson888 <philanderson888@hotmail.com>
2024-11-03 05:25:43 +0000

    update powershell notes

---
+++ random/2024-11-powershell.ps1
@@ -0,0 +1,14 @@
+see actual running script in the repository for the full code
+
+github actions 
+
+https://github.com/philanderson888/scripts/tree/master/.github/scripts
+
+github workflows
+
+https://github.com/philanderson888/scripts/tree/master/.github/workflows
+
+actual script being running
+
+https://github.com/philanderson888/scripts/blob/master/powershell/2024-10-powershell.ps1
+
